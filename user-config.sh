b0903619a8ecd8538b54166949e60e3f847f9fc1
Logan Waite <lwaite2010@gmail.com>
2016-06-14 16:34:19 -0600

    vagrant init

---
+++ user-config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+# sudo pip3 install virtualenvwrapper
+
+# load virtualenvwrapper
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=/vagrant
+export VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3'
+source /usr/local/bin/virtualenvwrapper.sh
+
+# create virtual environment
+mkvirtualenv -a $HOME opal_rewrite
+workon opal_rewrite
+
+# add to .bashrc to automatically enter virtual environment
+echo export WORKON_HOME=$HOME/.virtualenvs >> $HOME/.bashrc
+echo export PROJECT_HOME=/vagrant >> $HOME/.bashrc
+echo export VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3' >> $HOME/.bashrc
+echo source /usr/local/bin/virtualenvwrapper.sh >> $HOME/.bashrc
+echo workon django-rest-tutorial >> $HOME/.bashrc
+
+pip install -r /vagrant/requirements.txt
+
+# Set default directory to Django project folder
+echo cd /vagrant/tutorial/ >> $HOME/.bashrc
