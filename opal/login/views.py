517e7188c364e54805318fe9829d9758805ce19f
Logan Waite <lwaite2010@gmail.com>
2016-06-17 16:18:06 -0600

    starting login page

--- opal/login/views.py
+++ opal/login/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+def index(request):
+    return render(request, 'login/index.html')
+
+def sign_in(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # Redirect to a success page
+            return HttpResponse("Hello, World")
+        else:
+            # Return a 'disabled account' error messages
+        gelse:
+        # Return an 'invalid login' error message
