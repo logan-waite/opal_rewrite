451dc02e28bb7fdb3bc4057ed3d1d559c3e31688
Logan Waite <lwaite2010@gmail.com>
2016-07-01 18:09:59 -0600

    do events template

--- opal/cal/views.py
+++ opal/cal/views.py
@@ -1,7 +1,13 @@
+import calendar
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'cal/index.html')
+    calendar = createCalendar()
+    return render(request, 'cal/index.html', calendar)
+
+def createCalendar():
+    newCalendar = calendar.HTMLCalendar()
+    return newCalendar.formatmonth(2016, 7)
