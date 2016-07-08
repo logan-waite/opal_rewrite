703b4f2c7aa92b2c9bed9e6864738a6aa19ad394
Logan Waite <lwaite2010@gmail.com>
2016-07-07 18:17:46 -0600

    Mostly finished calendar view

--- opal/cal/views.py
+++ opal/cal/views.py
@@ -1,13 +1,24 @@
 import calendar
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 # Create your views here.
 @login_required
 def index(request):
     calendar = createCalendar()
-    return render(request, 'cal/index.html', calendar)
+    data = {'calendar':calendar}
+    data['month'] = 7
+    return render(request, 'cal/index.html', data)
 
 def createCalendar():
-    newCalendar = calendar.HTMLCalendar()
-    return newCalendar.formatmonth(2016, 7)
+    newCalendar = calendar.Calendar()
+    days = newCalendar.monthdatescalendar(2016, 7)
+    calendarDays = {}
+    i = 0
+
+    for x in days:
+        calendarDays[i] = x
+        i += 1
+
+    return calendarDays
