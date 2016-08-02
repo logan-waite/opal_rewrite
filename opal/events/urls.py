73cd8df2a8db09274d2cff07969396caf42f349e
Logan Waite <lwaite2010@gmail.com>
2016-08-02 16:58:42 -0600

    Updated event schedule form
    Added Jquery UI for the date picker
    finished schedule event form ui
    finished all of add place form.

--- opal/events/urls.py
+++ opal/events/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'new_event_form/$', views.new_event_form, name='new_event_form'),
     url(r'new_event_submit/$', views.new_event_submit, name='new_event_submit'),
     url(r'schedule_event_form/$', views.schedule_event_form, name='schedule_event_form'),
+    url(r'schedule_event_submit/$', views.schedule_event_submit, name='schedule_event_submit'),
+    url(r'add_place/$', views.add_place, name='add_place'),
 ]
--- opal/events/views.py
+++ opal/events/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from events.models import Event, Place
+from events.models import Event, Place, Scheduled_Event
 
 # Create your views here.
 @login_required
@@ -17,10 +17,6 @@ def new_event_submit(request):
     name = request.POST['name']
     description = request.POST['description']
     price = request.POST['price']
-
-    print(name)
-    print(description)
-    print(price)
 
     try:
         event = Event(
@@ -38,6 +34,57 @@ def new_event_submit(request):
 
 @login_required
 def schedule_event_form(request):
+    data = {}
+    data['events'] = Event.objects.order_by('name').all()
+    data['places'] = Place.objects.order_by('name').all()
+
+    return render(request, 'events/schedule_event_form.html', data)
+
+@login_required
+def schedule_event_submit(request):
+    event = request.POST['event']
+    place = request.POST['place']
+    from_date = request.POST['from']
+    to_date = request.POST['to']
+    try:
+        scheduled_event = Scheduled_Event(
+            event=event,
+            place=place,
+            start=from_date,
+            end=to_date
+        )
+
+        scheduled_event.save()
+
+        messages.success('Your event was sucessfully scheduled.')
+    except:
+        messages.error('Unable to schedule your event.')
+    return redirect('events:index')
+
+@login_required
+def add_place(request):
+    name = request.POST['name']
+    address = request.POST['address']
+    number = request.POST['number']
+    city = request.POST['city']
+    state = request.POST['state']
+    zipcode = request.POST['zip']
+
+    try:
+        place = Place(
+            name=name,
+            street_address=address,
+            room_number=number,
+            city=city,
+            state=state,
+            zipcode=zipcode
+        )
+
+        place.save()
+    except:
+        messages.error(request, "There was an error saving the place")
+        return redirect('events:index')
+
     data = {}
     data['events'] = Event.objects.order_by('name').all()
     data['places'] = Place.objects.order_by('name').all()
